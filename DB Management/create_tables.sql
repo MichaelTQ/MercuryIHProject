6cfbee47bf0dba10e397a70c9a6e58b2fddbecab
Qiang Tong <tongqiang.atp@gmail.com>
2014-07-10 10:08:42 -0400

    Added SQL Files
    create tables, insert sample data.

---
+++ DB Management/create_tables.sql
@@ -0,0 +1,131 @@
+-- Create table address
+create table address (
+	id number(10) primary key,
+	zip_code char(5),
+	street varchar2(20),
+	city varchar2(20),
+	state char(2)
+);
+
+-- Create table all_user
+-- type: 'c' means 'customers', 'a' means 'administrators'
+
+create table all_user (
+	id number(10) primary key,
+	username varchar2(20) not null,
+	email varchar2(40) not null,
+	phone varchar2(10),
+	first_name varchar2(20),
+	last_name varchar2(20),
+	password varchar2(50) not null,
+	address_id number(10),
+	type char(1) not null,
+	constraint all_user_address_fk
+		foreign key (address_id)
+		references address (id)
+);
+
+-- Create table card
+create table card (
+	id number(10) primary key,
+	card_number char(16) not null,
+	ex_month char(2),
+	ex_year char(4),
+	card_type varchar2(20)
+);
+
+-- Create table user_cards
+create table user_cards (
+	user_id number(10),
+	card_id number(10),
+	constraint user_cards_pk
+		primary key (user_id, card_id),
+	constraint user_cards_user_fk
+		foreign key (user_id) references all_user (id),
+	constraint user_cards_card_fk
+		foreign key (card_id) references card (id)
+);
+
+-- Create table station
+create table station (
+	id number(10) primary key,
+	name varchar2(20) not null,
+	state char(2),
+	city varchar2(20)
+);
+
+-- Create table ticket
+create table ticket (
+	id number(10) primary key,
+	order_number varchar2(10),
+	from_id number(10) not null,
+	to_id number(10) not null,
+	max_num number(4),
+	price number(4, 2),
+	start_time timestamp,
+	arrive_time timestamp,
+	constraint ticket_from_station_fk
+		foreign key (from_id) references station (id),
+	constraint ticket_to_station_fk
+		foreign key (to_id) references station (id),
+	constraint check_from_to check
+		(from_id > to_id or from_id < to_id)
+);
+
+-- Create table transaction
+create table transaction (
+	id number(10) primary key,
+	user_id number(10) not null,
+	ticket_id number(10) not null,
+	order_time timestamp,
+	constraint transaction_user_fk
+		foreign key (user_id) references all_user (id),
+	constraint transaction_ticket_fk
+		foreign key (ticket_id) references ticket (id)
+);
+
+-- Drop all the tables:
+drop table transaction;
+drop table ticket;
+drop table station;
+drop table user_cards;
+drop table card;
+drop table all_user;
+drop table address;
+
+-- Clear Data from all tables
+alter table transaction
+	disable constraint transaction_user_fk;
+alter table transaction
+	disable constraint transaction_ticket_fk;
+alter table ticket
+	disable constraint ticket_from_station_fk;
+alter table ticket
+	disable constraint ticket_to_station_fk;
+alter table user_cards
+	disable constraint user_cards_card_fk;
+alter table user_cards
+	disable constraint user_cards_user_fk;
+alter table all_user
+	disable constraint all_user_address_fk;
+truncate table address;
+truncate table all_user;
+truncate table card;
+truncate table user_cards;
+truncate table station;
+truncate table ticket;
+truncate table transaction;
+alter table all_user
+	enable constraint all_user_address_fk;
+alter table user_cards
+	enable constraint user_cards_user_fk;
+alter table user_cards
+	enable constraint user_cards_card_fk;
+alter table ticket
+	enable constraint ticket_to_station_fk;
+alter table ticket
+	enable constraint ticket_from_station_fk;
+alter table transaction
+	enable constraint transaction_ticket_fk;
+alter table transaction
+	enable constraint transaction_user_fk;
---
+++ DB Management/sample_data.sql
@@ -0,0 +1,116 @@
+-- address Sample data
+insert into address
+	values(0, '08540', '5 Independence Way','Princeton' ,'NJ');
+insert into address
+	values(1, '07030', '245 5th Street', 'Hoboken', 'NJ');
+
+-- all_user Sample data
+insert into all_user
+	values(0, 'test_user0', 'test@gmail.com', '2014449999',
+			'John', 'Doe', 'mercury', 0, 'c');
+insert into all_user
+	values(1, 'administrator', 'admin@gmail.com', '1043332222',
+			'Admin', 'Smith', 'mercury', 1, 'a');
+insert into all_user
+	values(2, 'admin', 'admin@aol.com', '2220004444',
+			'Admin', 'Howard', 'mercury', 0, 'a');
+
+-- card Sample data
+insert into card
+	values(0, '3714496353984312', '5', '2015', 'VISA');
+insert into card
+	values(1, '4111111111111111', '6', '2020', 'MASTER');
+insert into card
+	values(2, '5105105105105100', '12', '2017', 'MASTER');
+insert into card
+	values(3, '378282246310005', '8', '2040', 'AMERICAN EXPRESS');
+
+-- user_cards sample data
+insert into user_cards
+	values(0, 0);
+insert into user_cards
+	values(0, 1);
+insert into user_cards
+	values(0, 2);
+insert into user_cards
+	values(0, 3);
+
+-- station sample data
+insert into station
+	values(0, 'Princeton Junction', 'NJ', 'Princeton');
+insert into station
+	values(1, 'Hoboken Terminal', 'NJ', 'Hoboken');
+insert into station
+	values(2, 'Penn Station', 'NY', 'New York');
+insert into station
+	values(3, 'New Brunswick', 'NJ', 'New Brunswick');
+insert into station
+	values(4, 'Hudson', 'NY', 'Hudson');
+insert into station
+	values(5, 'Buffalo–Depew', 'NY', 'Buffalo');
+
+-- ticket sample data
+insert into ticket
+	values(0, 'ADAO42', 0, 5, 50, 70, 
+		(select current_timestamp from dual), null);
+insert into ticket
+	values(1, 'IHsB3D', 0, 1, 100, 10, null , null);
+insert into ticket
+	values(2, '84NBY3', 0, 2, 50, 25, null , null);
+insert into ticket
+	values(3, '0JB84S', 0, 3, 20, 7.5, null , null);
+insert into ticket
+	values(4, 'MX9SS3', 2, 4, 20, 20, null , null);
+insert into ticket
+	values(5, '9HVOQD', 2, 5, 40, 55, null , null);
+insert into ticket
+	values(6, 'YB7NBE', 3, 4, 20, 40, null , null);
+
+-- transaction sample data
+insert into transaction 
+	values (0, 0, 4,
+		(select current_timestamp from dual));
+insert into transaction 
+	values (1, 0, 2, null);
+insert into transaction 
+	values (2, 0, 0, null);
+insert into transaction 
+	values (3, 0, 3, null);
+insert into transaction 
+	values (4, 0, 6, null);
+insert into transaction 
+	values (5, 0, 2, null);
+
+-- Display all results properly.
+set numwidth 5;
+select * from address;
+
+column username format a10;
+column email format a10;
+column first_name heading FNAME;
+column last_name heading LNAME;
+column first_name format a8;
+column last_name format a8;
+column address_id heading ADDRESS;
+column password format a10;
+select * from all_user;
+
+set numwidth 5;
+select * from card;
+
+select * from user_cards;
+
+select * from station;
+
+column start_time format a15;
+column arrive_time format a15;
+select * from ticket;
+
+column order_time format a30;
+select * from transaction;
+
+-- Clear certain column format
+column column_name clear;
+
+-- Clear all columns
+clear columns
