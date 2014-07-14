eca58ae16c7ef2cd26c4e97c32a66052ce173c2f
Qiang Tong <tongqiang.atp@gmail.com>
2014-07-14 15:34:32 -0400

    Revert 0c48acc..123d47a
    This rolls back to commit 0c48accbd0ee9fab3b1d3b1e6dd4fe9dc58ee86f.

--- DB Management/create_tables.sql
+++ DB Management/create_tables.sql
@@ -1,50 +1,61 @@
+-- Drop all the tables:
+drop table transaction;
+drop table ticket;
+drop table station;
+--drop table user_cards;
+drop table card;
+drop table all_user;
+--drop table address;
+
 -- Create table address
-create table address (
-	id number(10) primary key,
-	zip_code char(5),
-	street varchar2(20),
-	city varchar2(20),
-	state char(2)
-);
+--create table address (
+--	id number(10) primary key,
+--	zip_code char(5),
+--	street varchar2(20),
+--	city varchar2(20),
+--	state char(2)
+--);
 
 -- Create table all_user
 -- type: 'c' means 'customers', 'a' means 'administrators'
 
 create table all_user (
 	id number(10) primary key,
-	username varchar2(20) not null,
-	email varchar2(40) not null,
+	username varchar2(20) not null unique,
+	email varchar2(40) not null unique,
 	phone varchar2(10),
 	first_name varchar2(20),
 	last_name varchar2(20),
 	password varchar2(50) not null,
-	address_id number(10),
 	type char(1) not null,
-	constraint all_user_address_fk
-		foreign key (address_id)
-		references address (id)
+	zip_code char(5),
+	street varchar2(20),
+	city varchar2(20),
+	state char(2)
 );
 
 -- Create table card
 create table card (
-	id number(10) primary key,
-	card_number char(16) not null,
+	card_number char(16) primary key,
+	user_id number(10),
 	ex_month char(2),
 	ex_year char(4),
-	card_type varchar2(20)
+	card_type varchar2(20),
+	constraint card_user_fk
+		foreign key (user_id) references all_user (id)
 );
 
 -- Create table user_cards
-create table user_cards (
-	user_id number(10),
-	card_id number(10),
-	constraint user_cards_pk
-		primary key (user_id, card_id),
-	constraint user_cards_user_fk
-		foreign key (user_id) references all_user (id),
-	constraint user_cards_card_fk
-		foreign key (card_id) references card (id)
-);
+--create table user_cards (
+--	user_id number(10),
+--	card_id number(10),
+--	constraint user_cards_pk
+--		primary key (user_id, card_id),
+--	constraint user_cards_user_fk
+--		foreign key (user_id) references all_user (id),
+--	constraint user_cards_card_fk
+--		foreign key (card_id) references card (id)
+--);
 
 -- Create table station
 create table station (
@@ -84,14 +95,6 @@ create table transaction (
 		foreign key (ticket_id) references ticket (id)
 );
 
--- Drop all the tables:
-drop table transaction;
-drop table ticket;
-drop table station;
-drop table user_cards;
-drop table card;
-drop table all_user;
-drop table address;
 
 -- Clear Data from all tables
 alter table transaction
@@ -102,25 +105,29 @@ alter table ticket
 	disable constraint ticket_from_station_fk;
 alter table ticket
 	disable constraint ticket_to_station_fk;
-alter table user_cards
-	disable constraint user_cards_card_fk;
-alter table user_cards
-	disable constraint user_cards_user_fk;
-alter table all_user
-	disable constraint all_user_address_fk;
-truncate table address;
+alter table card
+	disable constraint card_user_fk;
+--alter table user_cards
+--	disable constraint user_cards_card_fk;
+--alter table user_cards
+--	disable constraint user_cards_user_fk;
+--alter table all_user
+--	disable constraint all_user_address_fk;
+--truncate table address;
 truncate table all_user;
 truncate table card;
-truncate table user_cards;
+--truncate table user_cards;
 truncate table station;
 truncate table ticket;
 truncate table transaction;
-alter table all_user
-	enable constraint all_user_address_fk;
-alter table user_cards
-	enable constraint user_cards_user_fk;
-alter table user_cards
-	enable constraint user_cards_card_fk;
+--alter table all_user
+--	enable constraint all_user_address_fk;
+--alter table user_cards
+--	enable constraint user_cards_user_fk;
+--alter table user_cards
+--	enable constraint user_cards_card_fk;
+alter table card
+	enable constraint card_user_fk;
 alter table ticket
 	enable constraint ticket_to_station_fk;
 alter table ticket
